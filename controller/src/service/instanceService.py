9cffd44ece60015e970ffd123c466cd0732715df
Pedro Henrique Lopes <56567741+pedro-hbl@users.noreply.github.com>
2022-10-14 19:37:18 -0300

    decide between database or fopen

--- controller/src/service/instanceService.py
+++ controller/src/service/instanceService.py
@@ -30,4 +30,25 @@ class InstanceService:
             else:
                 filenum = filenum - 1
 
-        jstring = ""        
+        jstring = ""
+
+        graphs_dict = {} # holds the file name as key and graph as value,  the graph name will be the file name
+        graphs_size = {}
+        graphs_names = {} # holds every name of every schema
+
+        for x in range(0, filenum - 1):
+            graphs_dict.update({filename[x]:nx.Graph()})
+            graphs_size.update({filename[x]: 0})
+
+        node_number = 0
+        
+        for x in range(0, filenum - 1):
+            node_number = 0
+            try:
+                with open(filename[x], "r") as f:
+                    jstring = f.read()
+                jstring = removeSpaces(jstring)
+                final = gr.Pro(jstring, 0, 0, 0, graphs_dict.get(filename[x]))
+                graphs_size.update({filename[x]:final[3]})
+            except FileNotFoundError:   #Ja foi verificado, mas vale deixar aqui ainda
+                print("Arquivo %s não encontrado." % filename[x])#Ja criou o filename no dict
